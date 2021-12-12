efc876cab951ec23f8935386c8147d3c3d8717f4
EvgenyTom <94161689+EvgenyTom@users.noreply.github.com>
2021-12-12 15:24:23 +0300

    Create Zadanie_03_12_21_num_3.cs

---
+++ Zadanie_03_12_21_num_3.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+class Program
+
+{
+    static void Main(string[] args)
+    {
+        // Задание 3 Дата: 3 декабря
+        // Найти все целые числа из промежутка от 1 до 300, у которых ровно пять делителей.
+        for (int i = 1; i <= 300; i++)
+        {
+            int deliteli = 0;
+            for (int j = 1; j <= i; j++)
+            {
+                if (i % j == 0)
+                {
+                    deliteli += 1;
+                }
+            }
+            if (deliteli == 5)
+            {
+                Console.WriteLine($"Кол-во делителей числа {i} равно " + deliteli.ToString());
+            }
+        }
+    }
+}
